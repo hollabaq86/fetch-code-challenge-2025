13601034422d92c7e8032feffe1d1188627675dc
hollabaq86 <hollis65@me.com>
2025-02-11 17:18:01 -0600

    calculate points for a receipt

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
 from flask import Flask
+from flask import request
+from models.receipt import Receipt
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+receipts = {}
+
+@app.post("/receipts/process")
+def process():
+    datum = request.get_json()
+    if "retailer" in datum.keys():
+        receipt = Receipt(datum)
+        receipts[receipt.id] = receipt
+    return {"id": receipt.id}
+
+@app.get("/receipts/<id>/points")
+def get_points(id):
+    receipt_id = id
+    return {"points": receipts[receipt_id].points}
+
+def create_app():
+    return app
